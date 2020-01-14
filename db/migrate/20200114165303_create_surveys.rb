96754470826535bc2dd8ccd486ffd7df285a6d0f
Student <example@example.com>
2020-01-14 09:42:48 -0800

    Add db connections and migrations

---
+++ db/migrate/20200114165303_create_surveys.rb
@@ -0,0 +1,8 @@
+class CreateSurveys < ActiveRecord::Migration[6.0]
+  def change
+    create_table :surveys do |t|
+      t.column(:topic, :varchar)
+      t.timestamps()
+    end
+  end
+end
---
+++ db/migrate/20200114165313_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.column(:text, :varchar)
+      t.column(:answer1, :varchar)
+      t.column(:answer2, :varchar)
+      t.column(:answer3, :varchar)
+      t.column(:answer4, :varchar)
+      t.column(:survey_id, :integer)
+      t.timestamps()
+    end
+  end
+end
